1ceb684015796a1d93906447cf3a5a997823a864
kojin12 <bogdanivanov141084@gmail.com>
2026-03-18 23:42:25 +0300

    Подключение к бд

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"log"
-	routes "main/routes"
-	"net/http"
-)
+import dbconnection "main/dataBase/DBconnection"
 
 func main() {
-	router := routes.GetNewRouter()
-	
-	log.Println("server started :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	dbconnection.CheckConnection()
 }
